3252a3d5fe99105b2c98b25d96544637e92670b4
Adam Chainz <adam@adamj.eu>
2015-12-20 23:14:00 +0000

    Day 10

---
+++ days/10a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+def main():
+    string = '1113222113'
+    for i in range(40):
+        string = expand(string)
+    print(len(string))
+
+
+def expand(string):
+    current = string[0]
+    count = 1
+    expanded = ''
+    for char in string[1:]:
+        if char == current:
+            count += 1
+        else:
+            expanded += str(count) + current
+            count = 1
+            current = char
+    expanded += str(count) + current
+    return expanded
+
+
+if __name__ == '__main__':
+    main()
